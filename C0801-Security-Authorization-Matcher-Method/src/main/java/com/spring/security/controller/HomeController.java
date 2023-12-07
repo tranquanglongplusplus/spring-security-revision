03a744a0703eeaecc994b03322e0ed475ac67220
tranquanglongplusplus <tranquanglongplusplus@gmail.com>
2023-12-07 17:35:07 +0700

    C0801-Security-Authorization-Matcher-Method

---
+++ C0801-Security-Authorization-Matcher-Method/src/main/java/com/spring/security/controller/HomeController.java
@@ -0,0 +1,21 @@
+package com.spring.security.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello!";
+    }
+    @GetMapping("/ciao")
+    public String ciao() {
+        return "Ciao!";
+    }
+
+    @GetMapping("/hola")
+    public String hola() {
+        return "Hola!";
+    }
+}
