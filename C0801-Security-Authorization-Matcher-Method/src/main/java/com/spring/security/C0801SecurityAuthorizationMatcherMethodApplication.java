712f16505391cd40f20320d8926be525c9c54b34
tranquanglongplusplus <tranquanglongplusplus@gmail.com>
2023-12-07 14:32:21 +0700

    C0801-Security-Authorization-Matcher-Method

---
+++ C0801-Security-Authorization-Matcher-Method/src/main/java/com/spring/security/C0801SecurityAuthorizationMatcherMethodApplication.java
@@ -0,0 +1,13 @@
+package com.spring.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C0801SecurityAuthorizationMatcherMethodApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(C0801SecurityAuthorizationMatcherMethodApplication.class, args);
+    }
+
+}
---
+++ C0801-Security-Authorization-Matcher-Method/src/test/java/com/spring/security/C0801SecurityAuthorizationMatcherMethodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class C0801SecurityAuthorizationMatcherMethodApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
